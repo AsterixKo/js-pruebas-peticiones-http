0a38f0491c36b1ce29f33a25889500239cf23384
MSI\moneo <jvr.moneo@gmail.com>
2020-10-13 15:04:01 +0200

    parte 1

--- xmlHttpRequest.js
+++ xmlHttpRequest.js
@@ -1,1 +1,14 @@
 let xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://randomuser.me/api/?results=1');
+xhr.send();
+// GET --> Obtener información
+// POST --> Publicar o enviar información
+
+xhr.onload = function() {
+    if(xhr.status != 200){
+        console.log(`Error ${xhr.status}`);
+    }else{
+        console.log(`Respuesta ${xhr.response}`);
+    }
+};
